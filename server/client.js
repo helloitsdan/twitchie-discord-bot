73deb4958436cbdfe0a436238eb86d7bacee7365
dani quinn sexton <spells@witch.zone>
2018-03-24 22:11:10 +0000

    Add basic profile setup to client ready callback

--- server/client.js
+++ server/client.js
@@ -1,13 +1,30 @@
+const path = require('path')
+
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
 const streamEvents = require('./events')
+
+const avatarURL = path.join(__dirname, '..', 'assets', 'avatar.png')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
+
+  client.user.setAvatar(avatarURL)
+  client.user.setActivity(
+    `for streams!`,
+    {
+      type: 'WATCHING',
+    },
+  );
+})
+
+client.on('message', () => {
+
 })
 
 streamEvents.on('live', () => {
-  console.log('someone is live')
+  console.log('Someone is live!')
 })
 
 client.login(process.env.TWITCHIE_CLIENT_TOKEN)
